60d267aae5c1310a170efb3debd54cee3f6159b2
alejandrocora <alzapicolara@gmail.com>
2023-01-18 20:53:04 +0100

    general fixes

--- site_email_scraper/app.py
+++ site_email_scraper/app.py
@@ -6,7 +6,7 @@ from html import unescape
 
 DEFAULT_HEADER = 'Mozilla/5.0 (X11; Linux x86_64; rv:105.0) Gecko/20100101 Firefox/105.0'
 
-class site_browser:
+class SiteBrowser:
     def __init__(self, url):
         self.url = url
         parsed_url = urllib.parse.urlparse(url, scheme='', allow_fragments=True)
@@ -19,35 +19,40 @@ class site_browser:
             'User-agent':DEFAULT_HEADER
         })
 
-    def get_emails(self, text):
+    def dispatch_response(self, url):
+        html = unescape(self.session.get(url).text)
+        soup = BeautifulSoup(html, 'html.parser')
+        return [html, soup]
+
+    def get_site_emails(self, text):
         email_pattern = r'[a-zA-Z0-9._%+-]+@'+self.domain
-        return re.findall(email_pattern, text)
+        return list(set(re.findall(email_pattern, text)))
+
+    def get_site_paths(self, soup):
+        return (link.get('href') for link in soup.find_all('a') if self.domain in str(urllib.parse.urlparse(link.get('href'), scheme='', allow_fragments=True).netloc))
 
     def scrap_emails(self, depth):
-        html = unescape(self.session.get(self.url).text)
-        soup = BeautifulSoup(html, 'html.parser')
-        self.emails = list(set(self.get_emails(html)))
-        new_paths = [link.get('href') for link in soup.find_all('a') if self.domain in str(urllib.parse.urlparse(link.get('href'), scheme='', allow_fragments=True).netloc)]
-        old_paths = []
-        while depth != 0:
-            paths = new_paths
+        html, soup = self.dispatch_response(self.url)
+        self.emails = self.get_site_emails(html)
+        new_paths = self.get_site_paths(soup)
+        old_paths = [self.url]
+        while depth > 0:
+            paths = list(set([path for path in new_paths if path not in old_paths]))
             new_paths = []
             for path in paths:
-                print(path)
+                html, soup = self.dispatch_response(path)
+                self.emails = self.emails + self.get_site_emails(html)
+                for link in self.get_site_paths(soup):
+                    if link not in old_paths:
+                        new_paths.append(link)
                 old_paths.append(path)
-                path_html = self.session.get(path).text
-                soup = BeautifulSoup(path_html, 'html.parser')
-                self.emails = self.emails + self.get_emails(path_html)
-                for link in soup.find_all('a'):
-                    href = link.get('href')
-                    if (self.domain in str(urllib.parse.urlparse(href, scheme='', allow_fragments=True).netloc)) and (href not in old_paths):
-                        new_paths.append(href)
+            old_paths = list(set(old_paths))
             depth = depth - 1
         return list(set(self.emails))
 
-    #def get_site_links():
-
 def main():
+    a = SiteBrowser('https://google.com/')
+    print(a.scrap_emails(0))
 
 if __name__ == '__main__':
     main()
